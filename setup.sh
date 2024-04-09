579bd4d0dbafa1c407b6b4fd1a516de850d14194
qingpowuwu <qingpowuwu.study@gmail.com>
2024-04-09 13:57:45 +0800

    update

--- setup.sh
+++ setup.sh
@@ -6,11 +6,11 @@ pip install tensorflow==2.8.4
 # Install the learned optimization package from GitHub
 pip install git+https://github.com/google/learned_optimization.git
 
-# Install Jax
-pip install -U "jax==0.4.26[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# # Install Jax
+# pip install -U "jax==0.4.26[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
-# Install dm-haiku from GitHub
-pip install git+https://github.com/deepmind/dm-haiku
+# # Install dm-haiku from GitHub
+# pip install git+https://github.com/deepmind/dm-haiku
 
 
 # Install other dependencies
